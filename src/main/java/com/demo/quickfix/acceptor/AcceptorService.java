68014d2d695352531a24822f4e870c652d820609
Anthony <zamora.anthony1234@gmail.com>
2025-11-27 10:38:22 -0500

    Created AcceptorService class

---
+++ src/main/java/com/demo/quickfix/acceptor/AcceptorService.java
@@ -0,0 +1,54 @@
+package com.demo.quickfix.acceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import quickfix.*;
+import quickfix.fix42.MessageCracker;
+
+@Slf4j
+@Service
+public class AcceptorService extends MessageCracker implements Application {
+
+    @Override
+    public void onCreate(SessionID sessionID) {
+        // Executed when QuickFIX/J creates a new session
+        log.info("Session created {}", sessionID);
+    }
+
+    @Override
+    public void onLogon(SessionID sessionID) {
+        // Func gets executed when counterparty successfully login
+        log.info("Logon successful {}", sessionID);
+    }
+
+    @Override
+    public void onLogout(SessionID sessionID) {
+        // Notifies when a FIX session goes offline
+        log.info("Logout successful {}", sessionID);
+    }
+
+    @Override
+    public void toAdmin(Message message, SessionID sessionID) {
+        // Handles admin messages sent to counterparty
+        log.info("To admin {}", sessionID);
+    }
+
+    @Override
+    public void fromAdmin(Message message, SessionID sessionID) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, RejectLogon {
+        // Handles admin message sent from counterparty
+        log.info("From admin {}", sessionID);
+    }
+
+    @Override
+    public void toApp(Message message, SessionID sessionID) throws DoNotSend {
+        // Callback func that executes right before sending an app-level message to counterparty
+        // IMPORTANT: App-level and admin-level messages are different
+        log.info("Sending Message {}", sessionID);
+    }
+
+    @Override
+    public void fromApp(Message message, SessionID sessionID) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, UnsupportedMessageType {
+        // Accepts app-level messages from counterparty
+        crack(message, sessionID);
+    }
+}
