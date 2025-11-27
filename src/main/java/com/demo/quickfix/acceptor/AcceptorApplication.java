4471a72ba8ab9c8fceb47f232f6a49461f2b9ccf
Anthony <zamora.anthony1234@gmail.com>
2025-11-26 21:27:30 -0500

    Initial commit

---
+++ src/main/java/com/demo/quickfix/acceptor/AcceptorApplication.java
@@ -0,0 +1,13 @@
+package com.demo.quickfix.acceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcceptorApplication.class, args);
+	}
+
+}
