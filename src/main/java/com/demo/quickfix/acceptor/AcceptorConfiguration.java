054af6fac53c1cabf2f15857bfcc00a1784001dd
Anthony <zamora.anthony1234@gmail.com>
2025-11-27 13:22:08 -0500

    Switched to automatic configuration file

--- src/main/java/com/demo/quickfix/acceptor/AcceptorConfiguration.java
+++ src/main/java/com/demo/quickfix/acceptor/AcceptorConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import quickfix.*;
 
-@Configuration
+//@Configuration
 public class AcceptorConfiguration {
 
     @Bean
