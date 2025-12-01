b11d981e3b45e4c5a8e55adf46e17390b5cbeb0c
Anthony <zamora.anthony1234@gmail.com>
2025-11-30 21:59:23 -0500

    Added Kafka configuration

--- src/main/java/com/demo/quickfix/acceptor/kafka/KafkaProducerConfig.java
+++ src/main/java/com/demo/quickfix/acceptor/kafka/KafkaProducerConfig.java
@@ -1,2 +1,36 @@
-package com.demo.quickfix.acceptor.kafka;public class KafkaProducerConfig {
+package com.demo.quickfix.acceptor.kafka;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${kafka.bootstrap-server.address}")
+    private String bootstrapServer;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> configMap = new HashMap<>();
+
+        configMap.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        configMap.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configMap.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(configMap);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
--- src/main/java/com/demo/quickfix/acceptor/kafka/KafkaTopicConfig.java
+++ src/main/java/com/demo/quickfix/acceptor/kafka/KafkaTopicConfig.java
@@ -1,2 +1,33 @@
-package com.demo.quickfix.acceptor.kafka;public class KafkaTopicConfig {
+package com.demo.quickfix.acceptor.kafka;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Value("${kafka.bootstrap-server.address}")
+    private String bootstrapServer;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        // Connect Kafka dependency to Kafka server
+        Map<String, Object> configMap = new HashMap<>();
+        configMap.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        return new KafkaAdmin(configMap);
+    }
+
+    @Bean
+    public NewTopic fixOrdersTopic() {
+        // NOTE: Kafka automatically creates topics when a bean returns NewTopic is initialized
+        return new NewTopic("FIX-Orders", 1, (short) 1);
+    }
 }
