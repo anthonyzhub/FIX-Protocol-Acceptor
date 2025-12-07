1bbcdccc2e5f4b58c0891e5c25553bc7a3852060
Anthony <zamora.anthony1234@gmail.com>
2025-12-06 22:11:29 -0500

    Updated KafkaService

--- src/main/java/com/demo/quickfix/acceptor/kafka/KafkaService.java
+++ src/main/java/com/demo/quickfix/acceptor/kafka/KafkaService.java
@@ -3,13 +3,22 @@ package com.demo.quickfix.acceptor.kafka;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import quickfix.fix42.ExecutionReport;
 
 @Slf4j
 @RequiredArgsConstructor
-public class KafkaConsumerService {
+public class KafkaService {
+
+    private static final String FIX_ORDER_TOPIC = "FIX-Orders";
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     @KafkaListener(topics = "FIX-Orders", groupId = "orders")
     public void listenGroupOrders(String message) {
         log.info("Received Message in group [Order]: {}", message);
+    }
+
+    public void sendKafkaMessage(ExecutionReport executionReport) {
+        kafkaTemplate.send(executionReport.toString(), FIX_ORDER_TOPIC);
     }
 }
